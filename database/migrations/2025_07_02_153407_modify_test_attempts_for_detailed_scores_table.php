0ecfde323926d25b419ec447bfe310e530dea215
andruchristo27 <e41211364@student.polije.ac.id>
2025-07-04 04:48:41 +0700

    feat: migrate

--- database/migrations/2025_07_02_153407_modify_test_attempts_for_detailed_scores_table.php
+++ database/migrations/2025_07_02_153407_modify_test_attempts_for_detailed_scores_table.php
@@ -12,12 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('test_attempts', function (Blueprint $table) {
-            $table->dropColumn('question_order');
-            $table->text('student_code')->nullable()->after('user_id');
-            $table->string('student_code_path')->nullable()->after('student_code');
-            $table->decimal('output_accuracy_score', 5, 2)->default(0)->after('student_code_path');
-            $table->decimal('structure_score', 5, 2)->default(0)->after('output_accuracy_score');
-            $table->renameColumn('score', 'total_score');
+            // $table->dropColumn('question_order');
+            // $table->text('student_code')->nullable()->after('user_id');
+            // $table->string('student_code_path')->nullable()->after('student_code');
+            // $table->decimal('output_accuracy_score', 5, 2)->default(0)->after('student_code_path');
+            // $table->decimal('structure_score', 5, 2)->default(0)->after('output_accuracy_score');
+            // $table->renameColumn('score', 'total_score');
         });
     }
 
